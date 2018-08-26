2c9b75895f98c2ce63bbe42cd22f02b477b431c2
raktimkashyap <37590992+raktimkashyap@users.noreply.github.com>
2018-08-26 13:51:17 +0530

    Add files via upload

---
+++ mainjs.js
@@ -0,0 +1,12 @@
+
+window.onload=function(){
+    
+const x = document.getElementById("icon_holder");
+
+x.addEventListener("click", toggle);
+function toggle(){
+    x.classList.toggle('activated');
+}
+
+
+}
